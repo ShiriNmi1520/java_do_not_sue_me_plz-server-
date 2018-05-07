0624cf18ea10a481726d5d966a826d5f6752c6b4
ShiriNmi1520 <s20026352@gmail.com>
2018-05-07 15:56:31 +0800

    Can write data to firebase now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+//router set
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let apis = require('./routes/apis');
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,19 +26,19 @@ app.use('/users', usersRouter);
 app.use('/api', apis);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/apis.js
+++ routes/apis.js
@@ -1,8 +1,69 @@
 let express = require('express');
 let router = express.Router();
+let firebase = require('firebase');
+
+//firebase config setup
+let firebaseconfig = {
+    apiKey: "<Your api>",
+    authDomain: "<Your database url>",
+    databaseURL: "<Your database url with https>",
+    storageBucket: "<Your storagebucket>"
+};
+
+// firebase setup
+firebase.initializeApp(firebaseconfig);
+let database = firebase.database();
+
+//define function
+function writeData(uid, name, email){
+    firebase.database().ref('users/' + uid).set({
+        username : name,
+        email : email
+    });
+}
+
+router.route('/images/:id')
+
+    .get((req, res) => {
+        res.json({
+            id: req.params.id,
+            message: "[Get] Api for image: " + req.params.id
+        });
+
+    })
+
+    .post((req, res) => {
+        res.json({
+            id: req.params.id,
+            message: "[Post] Api for image: " + req.params.id
+        })
+    })
+
+    .put((req, res) => {
+        res.json({
+            id: req.params.id,
+            message: "[Put] Api for image: " + req.params.id
+        })
+    })
+
+    .delete((req, res) => {
+        res.json({
+            id: req.params.id,
+            message: "[Delete] Api for image: " + req.params.id
+        })
+    });
+
+router.route('/images/write/:id')
+
+    .get((req, res) => {
+        let id = req.params.id,
+            message = "Welcome: " + id;
+        writeData(id, "test", "test");
+        res.send(message);
+    });
 
 router.get('/images', (req, res) => {
-    res.json({ message: "First API!"});
+    res.json({message: "First API!"});
 });
 
 module.exports = router;
